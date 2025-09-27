875d12d96d9e66331c00315de36fc1ad96f0461d
swagatkhanal <khanalswagat2@gmail.com>
2025-09-27 17:33:38 +0545

    string operations

---
+++ stringmethod.py
@@ -0,0 +1,7 @@
+course='swagat kumar khanal '  
+print(course.upper())          #for uppercase 
+print(course.lower())          # lower case 
+print(course.find('kumar'))    #find 
+print(course.find('g'))
+print(course.replace('w','z'))    # replace the character that you want 
+print(len(course ))
