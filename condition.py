5eab93eddcc95004a8938eeba208147faa0a9b10
swagatkhanal <khanalswagat2@gmail.com>
2025-10-04 19:14:37 +0545

    if condition

---
+++ condition.py
@@ -0,0 +1,17 @@
+is_hot = False
+is_cold =False
+if is_hot:
+    print("It's a hot day ")
+    print("drink plenty of water ")
+elif is_cold:
+    print("it's a cold day ")
+    print("wear warm clothes ")
+else:
+    print("it's a lovely day ")
+
+
+print("enjoy your day ")
+
+
+
+
