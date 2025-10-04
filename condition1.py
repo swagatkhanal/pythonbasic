4cee42836bbfcfc07cdbe1de70107cc8efb9a98b
swagatkhanal <khanalswagat2@gmail.com>
2025-10-04 19:15:12 +0545

    simple exercise

---
+++ condition1.py
@@ -0,0 +1,14 @@
+price_of_house=1000000
+has_goodcredit =False
+
+if has_goodcredit:
+   
+    down_payment=0.1*price_of_house
+
+else:
+    down_payment =0.2*price_of_house
+print(f"down payment:${down_payment}")
+
+
+
+
