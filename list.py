db42d804a531732697dad6592452b4fe2b122866
swagatkhanal <khanalswagat2@gmail.com>
2025-12-20 01:51:39 +0545

    data types

---
+++ list.py
@@ -0,0 +1,26 @@
+
+x=[14,12,3]   #lists
+print(x)
+print(type(x))
+print(x[2])
+x[2]=67
+print(x)
+
+b =3
+print(type(b))
+y='simpilearn'     # strings 
+print(y)
+print(type(y))
+
+z=3.14   #float 
+print(type(z))
+
+
+
+a=(4,8,6,)   #tuples
+print(a[1])
+print(type(a))
+
+f=open('first.py','r')
+print(type(f))
+
