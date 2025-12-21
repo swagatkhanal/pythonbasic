bb5ef92d1f9e7bea6b350859b7fd9f983a6a8e3f
swagatkhanal <khanalswagat2@gmail.com>
2025-12-21 23:27:21 +0545

    conversion

---
+++ conversion.py
@@ -0,0 +1,17 @@
+x="192"
+print((type(x)))
+
+
+x=int(x)
+print((type(x)))
+print(int(x))
+
+
+x=float(x)
+print((type(x)))
+print(float(x))
+
+
+x=complex(x)
+print(complex(x))
+print(complex(2,6))
