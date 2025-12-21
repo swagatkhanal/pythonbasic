7d855d9e5918f56f8c345fdd6bb960143837ef3c
swagatkhanal <khanalswagat2@gmail.com>
2025-12-21 23:57:49 +0545

    functionsandlists

---
+++ functions.py
@@ -0,0 +1,16 @@
+#some function in python
+x=-7.5
+print(abs(x))
+
+import math   # exponent 
+y=10
+print(math.exp(y))
+
+print(math.pi)   #value of pi 
+print(math.e)    #value of e 
+
+print(math.sqrt(9))  # square root 
+
+print(max(1,3,4,5,90))
+
+print(min(1,2,3,4,5,90))
---
+++ types of list.py
@@ -0,0 +1,20 @@
+num =[1,2,3,4]
+print(num)
+
+letter=['a','b','c','d']
+print(letter)
+print(letter[::2])
+
+stg=["get","certified","hand"]
+print(stg)
+print(stg[::-1])    #reverse
+
+
+mix=[1,6,"mix"]
+print(mix)
+print(mix[2])
+print(mix[1:2])
+
+
+mat=[[1,2],['a','b']]
+print(mat)
