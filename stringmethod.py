f6ed8450caa859e029d26fc319d2db35a2330f05
swagatkhanal <khanalswagat2@gmail.com>
2025-12-24 23:59:07 +0545

    string methods

--- stringmethod.py
+++ stringmethod.py
@@ -1,7 +1,17 @@
-course='swagat kumar khanal '  
+course='swagat kumar khanal'  
 print(course.upper())          #for uppercase 
 print(course.lower())          # lower case 
 print(course.find('kumar'))    #find 
 print(course.find('g'))
 print(course.replace('w','z'))    # replace the character that you want 
 print(len(course ))
+print(course.index('k'))
+
+x=course.split(" ")    #creates a list 
+print(x)
+print(course.rpartition(" kumar "))
+
+hello=" good morning"
+
+conc=course  +  hello     #concatenation
+print(conc)
