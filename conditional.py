cfc6f92b6ddb467712d12b3d27571b892241d48a
swagatkhanal <khanalswagat2@gmail.com>
2025-12-26 23:59:27 +0545

    nested condition

---
+++ conditional.py
@@ -0,0 +1,12 @@
+#nested 
+c=21
+if c<25:
+    if c%2==0:
+          print("c is a even number and it is less than 25 ")
+      
+    else:
+         print("c is a odd number less than 25 ")
+    
+
+else:
+    print("c is grater than 25 ")
