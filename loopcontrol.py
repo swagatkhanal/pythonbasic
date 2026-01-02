4330e0012b32490b744a0f44e9fa73e061d67d79
swagatkhanal <khanalswagat2@gmail.com>
2026-01-02 15:47:34 +0545

    loop control statement

---
+++ loopcontrol.py
@@ -0,0 +1,16 @@
+#break
+x="hey there . how are you ?"
+for i in x :
+    if i==".":
+        break
+    print(i,end="")
+
+
+#continue 
+for i in [1,13,56,4,6]   :
+    if i>10:
+        continue 
+   
+    print(i)
+
+    
